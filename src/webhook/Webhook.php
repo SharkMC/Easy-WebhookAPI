a6e06185f58a9657fe2123bcba39d00285f98764
ShqrkMC <160005502+SharkMC@users.noreply.github.com>
2024-12-30 21:32:05 +0900

    Webhook.php の更新

--- src/webhook/Webhook.php
+++ src/webhook/Webhook.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace webhook;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use webhook\WebhookTask;
 
 class Webhook extends PluginBase {
 
-    public static string $webhookUrl;
+    public static string $webhookUrl = "";
     public static string $title = "Webhook";
     public static string $contents = "Contents";
     public static string $color = "FFFFFF";
 
     public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public static function setWebhookUrl(string $webhookUrl): void {
@@ -34,27 +35,7 @@ class Webhook extends PluginBase {
     }
 
     public static function sendWebhook(): void {
-        if (isset(self::$webhookUrl)) {
-
-            $data = [
-                "embeds" => [
-                    [
-                        "title" => self::$title,
-                        "description" => self::$contents,
-                        "color" => hexdec(self::$color)
-                    ]
-                ]
-            ];
-
-            $ch = curl_init(self::$webhookUrl);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
-            curl_exec($ch);
-            curl_close($ch);
-        }
+        $task = new WebhookTask(self::$webhookUrl, self::$title, self::$contents, self::$color);
+        Server::getInstance()->getAsyncPool()->submitTask($task);
     }
 }
